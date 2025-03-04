10b1404903c1edd2a45eb509b0ddab9895757c74
zeeznn <zeeznn@proton.me>
2025-03-04 04:50:34 +0000

    change install

--- functions/tgUninstall.js
+++ functions/tgUninstall.js
@@ -1,7 +1,12 @@
 
 async function onRequest (context) {
-    if (request.headers.get('X-Telegram-Bot-Api-Secret-Token') !== context.env.INSTALLTOKEN) {
-        return new Response('Unauthorized', { status: 403 })
+    const req = await context.request.json();
+    const password = req.password;
+    if (!password) {
+        return new Response(JSON.stringify({ message: "Password is required" }), { status: 400 });
+    }
+    if (password !== context.env.INSTALLTOKEN) {
+        return new Response(JSON.stringify({ message: "Password is wrong" }), { status: 400 });
     }
 
     const r = await (await fetch(apiUrl('setWebhook', { url: '' }))).json()
